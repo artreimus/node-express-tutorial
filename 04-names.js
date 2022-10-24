0a0a4c6992aedc3fcf7bf6a5ced807fefc3a0be8
Arthur Reimus <arthurreimus1623@gmail.com>
2022-10-24 12:29:02 +0800

    Move files to basic folder

--- 04-names.js
+++
@@ -1,8 +0,0 @@
-// CommonJS, every file is a module (by default)
-// Modules - Encapsulated Code (rule: only share minimum)
-
-const secret = 'SUPER SECRET';
-const john = 'john';
-const peter = 'peter';
-
-module.exports = { john, peter };
